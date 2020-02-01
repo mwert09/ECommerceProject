7df3524a3e63619b53bfde618d985d59b36c14ba
Mert <mwert09@gmail.com>
2020-02-01 18:23:09 +0300

    Implemented firebase utils, included ability to store auth users into our firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -29,7 +29,7 @@ class SignIn extends React.Component{
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form obSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit}>
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required/>
                     <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required/>
                     <div className="buttons">
